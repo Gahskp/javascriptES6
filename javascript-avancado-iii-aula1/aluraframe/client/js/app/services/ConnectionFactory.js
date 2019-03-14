7a05b503c1cfabc1f2433d1bb0d0a8c3115df7db
Gabriel Augusto de Oliveira <gahskp@gmail.com>
2019-03-14 10:53:37 -0300

    connection factory and monkey patch

--- javascript-avancado-iii-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
+++ javascript-avancado-iii-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,32 +1,56 @@
-var stores = ['negociacoes'];
-var version = 3;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function () {
 
-class ConnectionFactory {
+    var stores = ['negociacoes'];
+    var version = 3;
+    var dbName = 'aluraframe';
 
-    constructor(){
-        Throw new Error("ConnectionFactory não pode ser instanciada");
-    }
+    var connection = null;
+    var close = null;
 
-    static getConnection(){
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
+    return class ConnectionFactory {
 
-            openRequest.onupgradeneeded = e => {
+        constructor(){
+            throw new Error("ConnectionFactory não pode ser instanciada");
+        }
 
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                }
+
+                openRequest.onsuccess = e => {
+                    if(!connection){
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                              throw new Error('Você não pode fechar diretamente a conexão');
+                        };
+                    }
+                    resolve(connection);
+                }
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                }
+            });
+        }
+
+        static closeConnection(){
+            if (connection) {
+                close();
+                connection = null;
             }
+        }
 
-            openRequest.onsuccess = e => {
-
-            }
-
-            openRequest.onerror = e => {
-
-            }
-        });
+        static _createStores(connection){
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
     }
-
-    static _createStores(){
-        
-    }
-}
+}) ();
